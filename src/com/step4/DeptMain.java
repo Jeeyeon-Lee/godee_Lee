7c71a921f0a552d24d2eeb10c3ce991e7d5722a2
Jeeyeon-Lee <jeeyeon118@gmail.com>
2023-10-05 12:03:42 +0900

    메세지

--- src/com/step4/DeptMain.java
+++ src/com/step4/DeptMain.java
@@ -1,22 +1,57 @@
 package com.step4;
 class Dept{
 	//전변은 초기화를 생략할 수 있다. 생성자가 대신 해준다.
+	public Dept() {}
 	private int deptno;     //부서번호
 	private String dname; //부서명
 	private String loc;      //지역
-	public Dept() {}
 	public Dept(int deptno, String dname, String loc) {
 		//지역변수와 전역변수의 동기화! (생성자의 파라미터값으로 재정의, 초기화)
 		this.deptno  = deptno; 
 		this.dname  = dname;
 		this.loc       = loc;
 	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
 }
 public class DeptMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//인스턴스화를 해도 안된다. -> 캡슐레이션(캡슐화) 그 사물을 정의하는 중요한 고유명사 만들기 private
+		Dept dept   = new Dept();                                //기본값
+		Dept dept1 = new Dept(10, "개발1팀", "부산");    //10 새로운 객체 생성 
+		System.out.println(dept.getDeptno());                 //0
+		dept1.setDeptno(100);                                     //10->100 초기화 by 생성자의 인터셉트
+		System.out.println(dept1.getDeptno());               //10
+		dept1 = null;                                                 //캔디데이트 상태 -> 너는 앞으로 쓰레기 값이 됨 
+						                                                   //- > dept1 = new dept1();  실행되면 앞의 38번에서 생성된 객체는 사라짐 
+//		dept1.setDeptno(100);                                     //NullPointerException 발생
+		dept1 = new Dept();
+		dept1.setDeptno(100);                                     //10->100 초기화 by 생성자의 인터셉트
+		dept1.setDname("운영1팀");
+		dept1.setLoc("제주");
+		System.out.println(dept1.getDeptno()+", "+dept1.getDname()+", "+dept1.getLoc());
+		System.out.println(dept.getDeptno()+", "+dept.getDname()+", "+dept.getLoc());
 	}
-
 }
+/*
+Dept 클래스에 Getter Setter를 두는 것은 결국 전역변수를 사용하기 위함이다. 
+web서비스 - 동기화 가능
+150명 사용 -> 인스턴스를 갖는다 ->하나임 ->싱글톤
+*/
--- src/com/step4/GView.java
+++ src/com/step4/GView.java
@@ -1,20 +1,26 @@
 package com.step4;
+//16-20-(12-13-5-6-7)-22-(반복)
 class GLogic{
 	int i = 1;
 	public GLogic(GView gv) {  //null
-		System.out.println(gv);
+		System.out.println("GLogic : "+gv);
+		System.out.println("GLogic i : "+gv.j);
 	}
 }
 public class GView {
-	//디폴트 생성자만 생략 가능함!
-	GView() {
+	int j = 1;
+	GView() {	//디폴트 생성자만 생략 가능함!
 		GLogic gl = new GLogic(this);          //this는 현재 스레드가 관리하는 클래스이고 원본 나 자신
 		//원본인지 확인하는 법? 전역변수로 확인 가능??
 	}
 	public static void main(String[] args) {
 		//static 안에서는 this나 supper(부모클래스 수정자?) 사용 불가 -> 생성자에서는 가능하다
-		GView gv = new GView();
-		System.out.println(gv);
+		//이 때 this는 18번 거일까? 20번 거일까?(주소번지 다름 -> 다른값 들어갈 수 있음 -> 좋지 않은 방법)
+		//해결? static으로 하거나, 싱글톤으로 처리(메모리 누수 막기)
+		GView gv = new GView(); //생성자를 첫번째 호출
+		System.out.println("GView 1 : "+gv);
+		gv = new GView();           //생성자를 두번째 호출
+		gv.j = 10;
+		System.out.println("GView 2 : "+gv);
 	}
-
 }
