2841b4f4444574ce6cc83a510c0f5dfc4a68f9db
Jeeyeon-Lee <jeeyeon118@gmail.com>
2023-10-23 10:37:00 +0900

    20231023

---
+++ src/collec/list/ListMain1.java
@@ -0,0 +1,26 @@
+package collec.list;
+
+import java.util.List;
+class List1{
+	/*선언부 읽혀지는 순간은 생성자가 호출이 될 때이다. 
+	 * 초기화문제 -> 시점[타임라인관계] -> 인터셉트 가능(관여)*/
+	List<Integer> nums = null;
+	/*생성자*/
+	
+	/*정의메소드*/
+	
+	/*메인메소드*/
+	
+}
+public class ListMain1 {
+	/*선언부*/
+	/*생성자*/
+
+	/*정의메소드*/
+
+	/*메인메소드*/
+	public static void main(String[] args) {
+
+	}
+
+}
